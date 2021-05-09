b886a01b7be74466292989bbf37c5a8809e1ad7d
Meera Chotai <62544870+meerachotai@users.noreply.github.com>
2021-05-09 13:28:07 +0530

    Changed to ID

--- helper_scripts/make_annot.R
+++ helper_scripts/make_annot.R
@@ -17,29 +17,16 @@ outprefix = opt$outprefix
 
 input = read.table(input, header=F, sep="\t", stringsAsFactors = F)
 
-# seqid feature type start end 
-# note: sequence numbering starting at 1
-# add ##gff-version 3 on top
-# seqid = vector()
-# start = vector()
-# end = vector()
-# id = vector()
-
-# for(i in 1:nrow(input)) {
-#   # seqid[i] = input[i,1]
-#   # end[i] = input[i,2]
-#   id[i] = paste0("ID=exon",i) # check with CLP
-# }
-
 seqid = input[,1]
 end = input[,2]
 start = rep(1, nrow(input))
 feature = rep("exon", nrow(input))
 fillers = rep(".", nrow(input))
 strand = rep("+", nrow(input))
-parent = paste0(rep("Parent:", nrow(input)),input[,1])
+# parent = paste0(rep("Parent:", nrow(input)),input[,1])
+parent = paste0(rep("ID=", nrow(input)),input[,1])
 
 cat("##gff-version 3\n",file=outprefix)
 annot = cbind(seqid, fillers, feature, start, end, fillers, strand, fillers, parent) #, input[,2])
 
-write.table(annot, paste0(outprefix, "_annot.gff3"), append=TRUE, row.names = F, quote = F, col.names = F, sep = "\t")
+write.table(annot, outprefix, append=TRUE, row.names = F, quote = F, col.names = F, sep = "\t")
