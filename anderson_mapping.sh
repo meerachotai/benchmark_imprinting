0fec2c5424d8f2ac8e6d8eeb67becdcd7bf04295
Meera Chotai <62544870+meerachotai@users.noreply.github.com>
2021-05-09 13:49:25 +0530

    Still in progress
    but mapping functionality is available

---
+++ anderson_mapping.sh
@@ -0,0 +1,188 @@
+# dependencies: htseqcount, hisat2
+# sample commands
+# module load hisat2
+# strainA="cviA"
+# strainB="cviB"
+# outdir="new_out"
+# fastq_dir="$( pwd )/$outdir/reads_simul/simul_"
+# refA="$( pwd )/$outdir/cviA_genome.fa"
+# refB="$( pwd )/$outdir/cviB_genome.fa" 
+# annotA="$( pwd )/$outdir/cviA_annot.gff3"
+# annotB="$( pwd )/$outdir/cviB_annot.gff3"
+# $scripts_dir/anderson_mapping.sh -A $strainA -B $strainB -x $refA -y $refB -X $annotA -Y $annotB -f $fastq_dir -o new_out -e -i ID -r 3 -d $scripts_dir
+
+# REQUIRED
+scripts_dir=""
+strainA=""
+strainB=""
+outdir=""
+refA=""
+refB=""
+annotA=""
+annotB=""
+fastq_dir=""
+
+# OPTIONAL
+gene_key=""
+
+htseq_i="ID"
+rep=3
+unedited=false
+
+while getopts "A:B:x:y:X:Y:d:r:o:g:f:e:" opt; do
+	case $opt in
+		A)	strainA="$OPTARG"
+			;;
+		B)	strainB="$OPTARG"
+			;;
+		x)	refA="$OPTARG"
+			;;
+		y)	refB="$OPTARG"
+			;;
+		X)	annotA="$OPTARG"
+			;;
+		Y)	annotB="$OPTARG"
+			;;	
+		d)	scripts_dir="$OPTARG"
+			;;
+		r)	rep="$OPTARG"
+			;;
+		o)	outdir="$OPTARG"
+			;;
+		i)	htseq_i="$OPTARG" # leave it to default if using simulated genome
+			;;
+		g) 	gene_key="$OPTARG"
+			;;
+		f)	fastq_dir="$OPTARG"
+			;;
+		e)	unedited=true # assumes it's already renamed add -e to edit using default func
+			;;
+	esac
+done
+
+# ---------------------- step 7: rename annot and ref ------------------
+
+# strain_name ref annot strain_type(A/B)
+rename_chr() { 
+	strain=$1
+	ref=$2
+	annot=$3
+	map=$4
+	
+	# making new temp files, not changing original
+	sed "s/^>/>${strain}_/g" $ref > $map/${strain}_genome.fa # any line that has > in it
+	sed "s/^/${strain}_/" $annot > $map/${strain}_annot.gff3 # start of line	
+	
+	head="##gff-version 3" # replace header again
+	sed -i "1s/.*/$head/" $map/${strain}_annot.gff3
+}
+
+# strainA strainB cross_name(AxB_rep/BxA_rep)
+map() { 
+	strainA=$1
+	strainB=$2
+	cross=$3
+	map=$4	
+	fastq_dir=$5
+	
+	# concatenate reads files for A,B
+	cat ${fastq_dir}${cross}_A.fq ${fastq_dir}${cross}_B.fq > ${map}/${strainA}_${strainB}_${cross}.fq
+	hisat2 -k 20 -S ${map}/${strainA}_${strainB}_${cross}_map.sam -x ${map}/concat_${strainA}_${strainB} ${map}/${strainA}_${strainB}_${cross}.fq
+}
+
+# -------------------- step 10: counting -------------------------
+
+# with Parent instead of ID for better counts file
+# strainA strainB cross_name(AxB/BxA)
+count() { 
+	strainA=$1
+	strainB=$2
+	cross=$3
+	map=$4
+	htseq_i=$5
+	
+	python -m HTSeq.scripts.count -s no -m union -a 0 -i ${htseq_i} -o ${map}/${strainA}_${strainB}_${cross}_count.sam ${map}/${strainA}_${strainB}_${cross}_map.sam ${map}/concat_${strainA}_${strainB}.gff3 > ${map}/counts_${strainA}_${strainB}_${cross}.txt	
+}
+
+displaytime () {
+  local T=$1
+  local D=$((T/60/60/24))
+  local H=$((T/60/60%24))
+  local M=$((T/60%60))
+  local S=$((T%60))
+  [[ $D > 0 ]] && printf '%d days ' $D
+  [[ $H > 0 ]] && printf '%d hours ' $H
+  [[ $M > 0 ]] && printf '%d minutes ' $M
+  [[ $D > 0 || $H > 0 || $M > 0 ]] && printf 'and '
+  printf '%d seconds\n' $S
+}
+
+#################
+### pipeline ####
+#################
+
+time_start=$(date)	# time run was started
+ts=$(date +%s)	# time run was started (in seconds)
+
+echo "Run start on: $time_start"
+
+printf "\nMake sure the following dependencies are loaded: hisat2, htseqcount\n"
+printf "\nSummary of calls:\n"
+printf "using reference genome files: ${refA}, ${refB}\n"
+printf "using annotation files: ${annotA}, ${annotB}\n"
+printf "outdirectory: ${outdir}\n\n"
+
+workdir=$( pwd )
+outdir=${workdir}/${outdir}
+
+mkdir -p $outdir
+mkdir ${outdir}/map
+map="${outdir}/map" # where intermediate files will be stored
+
+# preparing for concatenating:
+# renames the ids to give strain names, adds A or B as needed for HTseq count
+
+if [ "$unedited" == "true" ]; then
+	printf "Renaming chromosomes to match strainA/B...\n"
+	rename_chr $strainA $refA $annotA $map
+	refA="$map/${strainA}_genome.fa"
+	annotA="$map/${strainA}_annot.gff3"
+	
+	rename_chr $strainB $refB $annotB $map
+	refB="$map/${strainB}_genome.fa"
+	annotB="$map/${strainB}_annot.gff3"
+fi
+
+# concatenates annot, ref + builds index for hisat2 (note: indexing takes long, possibly should qsub)
+# NOTE: these annotations / refs need to be RENAMED with strainA and strainB names.
+cat $refA $refB > $map/concat_${strainA}_${strainB}.fa
+hisat2-build $map/concat_${strainA}_${strainB}.fa $map/concat_${strainA}_${strainB} # build index
+cat $annotA $annotB > $map/concat_${strainA}_${strainB}.gff3
+
+# concatenates FASTQ files from reciprocal directions, maps them
+# produces file strainA_strainB_cross.sam (for igv + HTseqcount)
+
+# REQUIRED:
+# give a 'fastq_dir' variable name that represents the start of the file name (including address to that file)
+# the file HAS TO END with _$cross_$rep_A.fq or _$cross_$rep_B.fq, example: simul_AxB_1_A.fq
+
+printf "Mapping...\n"
+for i in $(seq 1 1 $rep)
+do
+	map $strainA $strainB AxB_${i} $map $fastq_dir
+	map $strainA $strainB BxA_${i} $map $fastq_dir
+done
+
+printf "Counting...\n"
+# produces file strainA_strainB_cross_edit.sam (for igv)
+# produces counts file strainA_strainB_cross.txt
+for i in $(seq 1 1 $rep)
+do
+	count $strainA $strainB AxB_${i} $map $htseq_i
+	count $strainA $strainB BxA_${i} $map $htseq_i
+done
+
+te=$(date +%s); echo "Done. Time elapsed: $( displaytime $(($te - $ts)) )"
+
+# get syntelogs 'gene key' strainA_strainB_gene_key.txt
+# make_key $strainA $strainB $annotA $annotB # or provided from user
