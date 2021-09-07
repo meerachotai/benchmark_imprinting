5ea52a6d1f51f9012502ef8b87b013d3097f61ba
Meera Chotai <62544870+meerachotai@users.noreply.github.com>
2021-09-06 20:41:31 -0700

    Update read_config_imprint.py

--- config/read_config_imprint.py
+++ config/read_config_imprint.py
@@ -3,67 +3,61 @@
 import random
 import sys
 
-output_env = str(sys.argv[1])
+input_file = str(sys.argv[1])
+output_env = str(sys.argv[2])
 
 inputs = []
 complete = True
-input_file = "imprinting_config.txt"
 
 with open(input_file, 'r') as f:
-	for line in f:
-		if line.startswith("#"):
-			continue
-		elif line.startswith("\n") or line.isspace():
-			continue
-		else:
-			split_line = line.split('=')
-# 			print(split_line)
-			next_in = split_line[-1].strip()
-			if(next_in.isspace()):
-				next_in = ""
-			if(next_in.lower() == "true" or next_in.lower() == "false"):
-				next_in.lower()
-			inputs.append(next_in)
+     
+    for line in f:
+        if line.startswith("#"):
+            continue
+        elif line.startswith("\n") or line.isspace():
+            continue
+        else:
+            split_line = line.split('=')
+            next_in = split_line[1].strip()
+            if(next_in.isspace() or next_in == ''):
+                if(split_line[0].strip() == "SEED"): # for simulating
+                    seed = random.randint(1, 1234567)
+                    inputs.append(seed)
+                    print("Using randomly-generated seed", seed)
+                else:
+                    print(split_line[0], "is empty, fill in config file and try again")
+                    complete = False
+                    break
+            else:
+                inputs.append(next_in)
 
 file = open(output_env, "w")
 
 file.write("outdir="+inputs[0] + "\n")
 file.write("scripts_dir="+inputs[1]+"\n")
-
-file.write("strainA="+inputs[2]+"\n")
-file.write("strainB="+inputs[3]+"\n")
-file.write("fastq_dir="+inputs[4]+"\n")
-file.write("paired_end="+inputs[5]+"\n")
-
-file.write("picard="+inputs[6]+"\n")
-file.write("genome="+inputs[7]+"\n")
-file.write("annot="+inputs[8]+"\n")
-file.write("snps="+inputs[9]+"\n")
-file.write("mat_cutoff="+inputs[10]+"\n")
-file.write("pat_cutoff="+inputs[11]+"\n")
-file.write("paired="+inputs[12]+"\n")
-file.write("rep="+inputs[13]+"\n")
-file.write("AxB_rep="+inputs[14]+"\n")
-file.write("BxA_rep="+inputs[15]+"\n")
-file.write("majority="+inputs[16]+"\n")
-
-file.write("counts_dir="+inputs[17]+"\n")
-file.write("pval_wyder="+inputs[18]+"\n")
-file.write("logfc_wyder="+inputs[19]+"\n")
-
-file.write("refA="+inputs[20]+"\n")
-file.write("annotA="+inputs[21]+"\n")
-file.write("refB="+inputs[22]+"\n")
-file.write("annotB="+inputs[23]+"\n")
-file.write("gene_key="+inputs[24]+"\n")
-file.write("htseq_i="+inputs[25]+"\n")
-file.write("pval="+inputs[26]+"\n")
-file.write("mat_cutoff="+inputs[27]+"\n")
-file.write("pat_cutoff="+inputs[28]+"\n")
-file.write("logfc="+inputs[29]+"\n")
-file.write("rep="+inputs[30]+"\n")
-file.write("rename="+inputs[31]+"\n")
-file.write("a_annot="+inputs[32]+"\n")
-file.write("b_annot="+inputs[33]+"\n")
+file.write("ref="+inputs[2]+"\n")
+file.write("annot="+inputs[3]+"\n")
+file.write("strainA="+inputs[4]+"\n")
+file.write("strainB="+inputs[5]+"\n")
+file.write("refA="+inputs[6]+"\n")
+file.write("refB="+inputs[7]+"\n")
+file.write("annotA="+inputs[8]+"\n")
+file.write("annotB="+inputs[9]+"\n")
+file.write("seed="+inputs[10]+"\n")
+file.write("score="+inputs[11]+"\n")
+file.write("window="+inputs[12]+"\n")
+file.write("snp_score="+inputs[13]+"\n")
+file.write("indel_score="+inputs[14]+"\n")
+file.write("extend_score="+inputs[15]+"\n")
+file.write("match_score="+inputs[16]+"\n")
+file.write("ratio="+inputs[17]+"\n")
+file.write("total_n="+inputs[18]+"\n")
+file.write("unbiased="+inputs[19]+"\n")
+file.write("meg="+inputs[20]+"\n")
+file.write("peg="+inputs[21]+"\n")
+file.write("megs_bias="+inputs[22]+"\n")
+file.write("pegs_bias="+inputs[23]+"\n")
+file.write("rep="+inputs[24]+"\n")
+file.write("read_length="+inputs[25]+"\n")
 
 file.close()
