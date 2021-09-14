bd37f1788260eb40fb823f60a4dff2f7957192eb
Meera Chotai <62544870+meerachotai@users.noreply.github.com>
2021-09-13 23:34:10 -0700

    Update read_config_imprint.py

--- config/read_config_imprint.py
+++ config/read_config_imprint.py
@@ -2,11 +2,11 @@
 
 import random
 import sys
+import pandas as pd
 
 input_file = str(sys.argv[1])
 output_env = str(sys.argv[2])
 
-# input_file = "imprinting_config.txt"
 inputs = []
 complete = True
 
@@ -18,7 +18,6 @@ with open(input_file, 'r') as f:
 			continue
 		else:
 			split_line = line.split('=')
-# 			print(split_line)
 			next_in = split_line[-1].strip()
 			if(next_in.isspace()):
 				next_in = ""
@@ -36,35 +35,61 @@ file.write("strainB="+inputs[3]+"\n")
 file.write("fastq_dir="+inputs[4]+"\n")
 file.write("paired_end="+inputs[5]+"\n")
 
-file.write("picard="+inputs[6]+"\n")
-file.write("genome="+inputs[7]+"\n")
-file.write("annot="+inputs[8]+"\n")
-file.write("snps="+inputs[9]+"\n")
-file.write("mat_cutoff_picard="+inputs[10]+"\n")
-file.write("pat_cutoff_picard="+inputs[11]+"\n")
-file.write("paired="+inputs[12]+"\n")
-file.write("rep="+inputs[13]+"\n")
-file.write("AxB_rep="+inputs[14]+"\n")
-file.write("BxA_rep="+inputs[15]+"\n")
-file.write("majority="+inputs[16]+"\n")
+if(inputs[4] == ""):
+	if(inputs[6] == ""):
+		print("Please provide either a FASTQ directory prefix under FASTQ_DIR or provide a tab-delimited config file under FASTQ_CONFIG_FILE")
+		quit()
+	else:
+		config = pd.read_table(inputs[6],delimiter = "\t", header=None)
+		config = config.replace({config.columns[2]:'AxB'},1)
+		config = config.replace({config.columns[2]:'BxA'},2)
+		config = config.sort_values(by=[config.columns[3]], axis = 0)
+		config = config.sort_values(by=[config.columns[1]], axis = 0)
+		config = config.replace({config.columns[2]:'F'},1)
+		config = config.replace({config.columns[2]:'R'},2)
+		config = config.reset_index(drop=True)	
 
-file.write("counts_dir="+inputs[17]+"\n")
-file.write("pval_wyder="+inputs[18]+"\n")
-file.write("logfc_wyder="+inputs[19]+"\n")
+		strand = config[config.columns[2]].tolist()
+		if(2 in strand): # reverse file is given
+			file.write("paired_end=true\n")
+			for i,s in enumerate(strand):
+				if(i % 2):
+					if(s == 1):
+						b, c = config.iloc[i-1].copy(), config.iloc[i].copy()
+						config.iloc[i],config.iloc[i-1] = b,c
+		filenames = config[[config.columns[0]]]
+		filenames.to_csv("rearranged_config.txt",sep='\t', index=False, header=False)
+		file.write("config=rearranged_config.txt\n")
+        
+file.write("picard="+inputs[7]+"\n")
+file.write("genome="+inputs[8]+"\n")
+file.write("annot="+inputs[9]+"\n")
+file.write("snps="+inputs[10]+"\n")
+file.write("mat_cutoff_picard="+inputs[11]+"\n")
+file.write("pat_cutoff_picard="+inputs[12]+"\n")
+file.write("paired="+inputs[13]+"\n")
+file.write("rep="+inputs[14]+"\n")
+file.write("AxB_rep="+inputs[15]+"\n")
+file.write("BxA_rep="+inputs[16]+"\n")
+file.write("majority="+inputs[17]+"\n")
 
-file.write("refA="+inputs[20]+"\n")
-file.write("annotA="+inputs[21]+"\n")
-file.write("refB="+inputs[22]+"\n")
-file.write("annotB="+inputs[23]+"\n")
-file.write("gene_key="+inputs[24]+"\n")
-file.write("htseq_i="+inputs[25]+"\n")
-file.write("pval="+inputs[26]+"\n")
-file.write("mat_cutoff_anderson="+inputs[27]+"\n")
-file.write("pat_cutoff_anderson="+inputs[28]+"\n")
-file.write("logfc="+inputs[29]+"\n")
-file.write("rep="+inputs[30]+"\n")
-file.write("rename="+inputs[31]+"\n")
-file.write("a_annot="+inputs[32]+"\n")
-file.write("b_annot="+inputs[33]+"\n")
+file.write("counts_dir="+inputs[18]+"\n")
+file.write("pval_wyder="+inputs[19]+"\n")
+file.write("logfc_wyder="+inputs[20]+"\n")
+
+file.write("refA="+inputs[21]+"\n")
+file.write("annotA="+inputs[22]+"\n")
+file.write("refB="+inputs[23]+"\n")
+file.write("annotB="+inputs[24]+"\n")
+file.write("gene_key="+inputs[25]+"\n")
+file.write("htseq_i="+inputs[26]+"\n")
+file.write("pval="+inputs[27]+"\n")
+file.write("mat_cutoff_anderson="+inputs[28]+"\n")
+file.write("pat_cutoff_anderson="+inputs[29]+"\n")
+file.write("logfc="+inputs[30]+"\n")
+file.write("rep="+inputs[31]+"\n")
+file.write("rename="+inputs[32]+"\n")
+file.write("a_annot="+inputs[33]+"\n")
+file.write("b_annot="+inputs[34]+"\n")
 
 file.close()
