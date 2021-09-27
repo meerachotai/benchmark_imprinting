2f377c662d4c767e35c51851bc345a4c23f0cfa8
Meera Chotai <62544870+meerachotai@users.noreply.github.com>
2021-09-26 18:46:16 -0700

    Add files via upload

---
+++ benchmark/call_true_positives.R
@@ -0,0 +1,54 @@
+#!/usr/bin/env Rscript
+
+if (is.element('argparse', installed.packages()[,1])==FALSE) { stop("Error: R package argparse could not be loaded") }
+suppressPackageStartupMessages(library(argparse))
+
+parser = ArgumentParser()
+
+parser$add_argument("outdir", nargs=1, help = "outdirectory")
+
+args <- commandArgs(trailingOnly = TRUE)
+
+opt <- parser$parse_args()
+
+outdir = opt$outdir
+
+picard_megs = scan(paste0(outdir,"/picard_MEGs.txt"), character())
+picard_pegs = scan(paste0(outdir,"/picard_PEGs.txt"), character())
+
+wyder_megs = scan(paste0(outdir,"/wyder_MEGs.txt"), character())
+wyder_pegs = scan(paste0(outdir,"/wyder_PEGs.txt"), character())
+
+anderson_megs = scan(paste0(outdir,"/anderson_MEGs.txt"), character())
+anderson_pegs = scan(paste0(outdir,"/anderson_PEGs.txt"), character())
+
+true_megs = scan(paste0(outdir,"/true_MEGs.txt"), character())
+true_pegs = scan(paste0(outdir,"/true_PEGs.txt"), character())
+# all_genes = scan(paste0(outdir,"/all_genes.txt"), character())
+
+true_wyder_megs = intersect(true_megs, wyder_megs)
+true_wyder_pegs = intersect(true_pegs, wyder_pegs)
+cat("-----------------------------------\n")
+cat("total Wyder maternally-biased:", length(wyder_megs), "\n")
+cat("true Wyder maternally-biased: ",length(true_wyder_megs), "\n") 
+cat("total Wyder paternally-biased:", length(wyder_pegs), "\n")
+cat("true Wyder paternally-biased: ",length(true_wyder_pegs), "\n")
+cat("-----------------------------------\n")
+
+true_picard_megs = intersect(true_megs, picard_megs)
+true_picard_pegs = intersect(true_pegs, picard_pegs)
+cat("-----------------------------------\n")
+cat("total Picard maternally-biased:", length(picard_megs), "\n")
+cat("true Picard maternally-biased: ",length(true_picard_megs), "\n") 
+cat("total Picard paternally-biased:", length(picard_pegs), "\n")
+cat("true Picard paternally-biased: ",length(true_picard_pegs), "\n")
+cat("-----------------------------------\n")
+
+true_anderson_megs = intersect(true_megs, anderson_megs)
+true_anderson_pegs = intersect(true_pegs, anderson_pegs)
+cat("-----------------------------------\n")
+cat("total Anderson maternally-biased:", length(anderson_megs), "\n")
+cat("true Anderson maternally-biased: ",length(true_anderson_megs), "\n") 
+cat("total Anderson paternally-biased:", length(anderson_pegs), "\n")
+cat("true Anderson paternally-biased: ",length(true_anderson_pegs), "\n")
+cat("-----------------------------------\n")
