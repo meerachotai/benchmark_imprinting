709e10c291a8d13c238f80aae10def545104c506
Meera Chotai <62544870+meerachotai@users.noreply.github.com>
2022-03-21 11:22:05 -0500

    Update fig4.R

--- figures/fig4.R
+++ figures/fig4.R
@@ -1,24 +1,25 @@
-setwd("~/Documents/SJ_Lab/Imprinting/Winter2022/benchmark_sim_95/counts")
-
 library(stringr)
+library(ggplot2)
+library(patchwork)
 
-infile = "anderson_AxB_1.txt"
+indir = "benchmark_sim_90/counts" # for fig. 7, change to benchmark_sim_95/counts/
+infile = paste0(indir,"/anderson_AxB_1.txt")
+
 anderson = read.table(infile, header = F, sep = "\t", stringsAsFactors = F, row.names = 1)
 row.names(anderson) = str_remove(row.names(anderson), "strainA")
-row.names(anderson) = str_remove(row.names(anderson), "strainB")
 
-infile = "rep_1_1_strainAxstrainB_counts_merged.txt"
+infile = paste0(indir,"/rep_1_1_strainAxstrainB_counts_merged.txt")
 picard = read.table(infile, header = F, sep = "\t", stringsAsFactors = F, row.names = 1)
 
-infile = "simul_counts+id_A.txt"
+infile = paste0(indir,"/simul_counts+id_A.txt")
 trueA = read.table(infile, header = T, sep = "\t", stringsAsFactors = F, row.names = 1)
-trueA = trueA[,1, drop = FALSE]
+trueA = trueA[,1, drop = FALSE] # keep just the first replicate
 
-infile = "simul_counts+id_B.txt"
+infile = paste0(indir,"/simul_counts+id_B.txt")
 trueB = read.table(infile, header = T, sep = "\t", stringsAsFactors = F, row.names = 1)
 trueB = trueB[,1, drop = FALSE]
 
-true = merge(trueA, trueB, by = 0)
+true = merge(trueA, trueB, by = 0) # AxB_A and AxB_B - first replicate
 row.names(true) = true$Row.names
 true = subset(true, select = -c(Row.names))
 names(true) = c("mat.true", "pat.true")
@@ -34,10 +35,7 @@ names(all) <- gsub("V2",paste0("mat"),names(all),fixed = T)
 names(all) <- gsub("V3",paste0("pat"),names(all),fixed = T)
 
 all = merge(all, true, by = 0, all = TRUE)
-# row.names(all) = all$Row.names
 all = subset(all, select = -c(Row.names))
-
-library(ggplot2)
 
 # ---------------------- MATERNAL -----------------------------
 pval = vector()
@@ -78,7 +76,6 @@ two = ggplot(all, aes(x = mat.true, y = mat.picard)) +
   xlab("True") + ylab("Picard") +
   ggtitle("True vs. Picard maternal counts")
 
-
 wrap_plots(one, two, nrow = 1, ncol = 2)
 
 
@@ -92,7 +89,7 @@ for (i in 1:nrow(all)) {
   pval = c(pval, y)
 }
 adjp = p.adjust(pval, method = "fdr", n = length(pval))
-print(length(adjp[adjp < 0.05]))
+print(length(adjp[adjp < 0.05])) # significantly different
 
 sig = ifelse(adjp < 0.05 & !is.na(adjp), "maroon", "black")
 
@@ -121,5 +118,5 @@ four = ggplot(all, aes(x = pat.true, y = pat.picard)) +
   xlab("True") + ylab("Picard") +
   ggtitle("True vs. Picard paternal counts")
 
-wrap_plots(one, two, three, four, nrow = 2, ncol = 2)
+wrap_plots(three, four, nrow = 1, ncol = 2)
 
